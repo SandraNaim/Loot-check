6929a1e222aa5ad433a953c27f8c427f6268963d
Sandra Naim <sandra.naim@toothpickapp.com>
2022-01-19 14:07:16 +0200

    create the full redux flow test and code for the deposit and the withdra function

--- src/actions/balance.test.js
+++ src/actions/balance.test.js
@@ -6,3 +6,15 @@ it('create an action to set balance', () => {
     const expectedAction = { type: constants.SET_BALANCE, balance };
     expect(actions.setBalance(balance)).toEqual(expectedAction);
 })
+
+it('create an action to deposit into the balance', () => {
+    const deposit = 0;
+    const expectedAction = { type: constants.DEPOSIT, deposit };
+    expect(actions.deposit(deposit)).toEqual(expectedAction);
+})
+
+it('create an action to withdraw from the balance', () => {
+    const withdrawal = 10;
+    const expectedAction = { type: constants.WITHDRAW, withdrawal };
+    expect(actions.withdraw(withdrawal)).toEqual(expectedAction);
+})
--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -7,4 +7,18 @@ describe('balanceReducer', () => {
         const action = { type: constants.SET_BALANCE, balance }
         expect(balanceReducer(undefined, action)).toEqual(balance)
     })
+
+    it('deposit into the balance', () => {
+        const deposit = 10;
+        const initialState = 5;
+        const action = { type: constants.DEPOSIT, deposit }
+        expect(balanceReducer(initialState, action)).toEqual(initialState+deposit)
+    })
+
+    it('withdraw from the balance', () => {
+        const withdrawal = 10;
+        const initialState = 20;
+        const action = { type: constants.WITHDRAW, withdrawal }
+        expect(balanceReducer(initialState, action)).toEqual(initialState-withdrawal)
+    })
 })
