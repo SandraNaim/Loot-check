41eec4d54d8401ffa3479a8f6a8395105d887dc7
Sandra Naim <sandra.naim@toothpickapp.com>
2022-01-19 16:38:36 +0200

    add the input where the user can withdraw or deposit the wallet balance

--- src/component/Wallet.js
+++ src/component/Wallet.js
@@ -3,10 +3,23 @@ import { connect } from "react-redux";
 
 // export both the wallet component and the connect so we can test both
 export class Wallet extends Component {
+    constructor(){
+        super();
+        this.state = {
+            balance: undefined
+        }
+    }
+
+    updateBalanace = (evt) => {
+        this.setState({ balance: parseInt(evt.target.value, 10) })
+    }
+
     render(){
         return(
             <div>
                 <h3 data-test="balance">Wallet Balance: {this.props.balance}</h3>
+                <br />
+                <input data-test="input-wallet" onChange={this.updateBalanace} />
             </div>
         )
     }
--- src/component/Wallet.test.js
+++ src/component/Wallet.test.js
@@ -12,4 +12,21 @@ describe('Wallet', () => {
   it('displays the balance from props', () => {
       expect(wallet.find("[data-test='balance']").text()).toEqual('Wallet Balance: ');
   })
+
+  it('create an input to withraw or deposit the wallet', () => {
+    expect(wallet.find("[data-test='input-wallet']").exists()).toBe(true);
+  })
+
+  describe('when the user types into the wallet input', () => {
+    const userBalance = '25';
+
+    beforeEach(() => {
+      wallet.find("[data-test='input-wallet']")
+        .simulate('change', { target: { value: userBalance } })
+    })
+
+    it('update the local wallet balence in `state` and covert it to a number', () => {
+      expect(wallet.state().balance).toEqual(parseInt(userBalance, 10))
+    })
+  })
 })
