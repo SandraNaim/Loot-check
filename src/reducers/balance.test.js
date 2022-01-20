58bcf8f34c54ffa129e30d14acd6a20b50fd46d0
Sandra Naim <sandra.naim@toothpickapp.com>
2022-01-20 11:47:14 +0200

    store data with cookies using the sfcookies library

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -1,11 +1,21 @@
 import * as constants from '../actions/constants';
 import balanceReducer from './balance';
+import balanceReducer2 from './balance';
 
 describe('balanceReducer', () => {
-    it('sets a balance', () => {
+    describe('when initializing', () => {
         const balance = 10;
-        const action = { type: constants.SET_BALANCE, balance }
-        expect(balanceReducer(undefined, action)).toEqual(balance)
+
+        it('sets a balance', () => {
+            const action = { type: constants.SET_BALANCE, balance }
+            expect(balanceReducer(undefined, action)).toEqual(balance)
+        })
+
+        describe('then re-initializing', () => {
+            it('reads the balance from cookies', () => {
+                expect(balanceReducer2(undefined, {})).toEqual(balance)
+            })
+        })
     })
 
     it('deposit into the balance', () => {
